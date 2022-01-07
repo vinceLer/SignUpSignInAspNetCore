fcc6b395a6e52799ebee342742df8ff6a9a75d71
vince <vince.leroy@outlook.com>
2022-01-07 20:27:11 +0100

    Tools/DataContext initialized with package NuGets Microsoft.EntityFrameworkCore - Installation of packages NuGets Microsoft.EntityFramework.SqlServer & Microsoft.EntityFramework.Tools

---
+++ Tools/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignUpSignInAspNetCore.Models;
+
+namespace SignUpSignInAspNetCore.Tools
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDb)\v-app;Integrated Security=True");
+        }
+    }
+}
