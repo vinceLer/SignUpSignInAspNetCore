460ab2cb985853a52da6d5ad223d51a1aff9f0e3
vince <vince.leroy@outlook.com>
2022-01-07 20:18:05 +0100

    Creation of User (Model)

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace SignUpSignInAspNetCore.Models
+{
+    public class User
+    {
+        private int id;
+        private string userName;
+        private string password;
+        public User() { }
+        public int Id { get => id; set => id = value; }
+        public string UserName { get => userName; set => userName = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
